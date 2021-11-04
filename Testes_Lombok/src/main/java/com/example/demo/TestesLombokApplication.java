58908db9e5c202bd273a1a0a37e6b3b5b06352f8
Lucas Martins <lmartins333@hotmail.com>
2021-11-03 21:03:50 -0300

    Domain model, database seed

---
+++ Testes_Lombok/src/main/java/com/example/demo/TestesLombokApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesLombokApplication.class, args);
+	}
+
+}
